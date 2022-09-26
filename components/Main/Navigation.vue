f5ed51da02f029b38dfaee02fdace971eed35c15
Bayu Fanani <fanani707@gmail.com>
2022-09-26 23:02:53 +0700

    upgrade to nuxt js 2

---
+++ components/Main/Navigation.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3 class="text-xl">Hellow</h3>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <UnderConstruction />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  @apply bg-blue-50;
+}
+</style>
